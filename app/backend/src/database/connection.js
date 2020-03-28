1a0144fe0242e85eca7246d0a5a908e9b7a99a49
Arthur Olmos Wosniaki <arthur.wosniaki@totvs.com.br>
2020-03-28 19:30:27 -0300

    28Mar20 - Adicionando testes unitarios, integrados e validações ao backend

--- app/backend/src/database/connection.js
+++ app/backend/src/database/connection.js
@@ -1,7 +1,9 @@
 const knex = require('knex')
 const configuration = require('../../knexfile')
 
+const env = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development
+console.log(env)
 
-const connection = knex(configuration.development)
+const connection = knex(env)
 
 module.exports = connection
